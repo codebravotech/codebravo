0210cfa7b7e41520409dba908a1b5b63b3b6cded
CodeBRAVO <johnnypovolny@gmail.com>
2025-01-15 09:43:34 -0800

    Fix animation issue

--- src/animations/ArriveUpwards.tsx
+++ src/animations/ArriveUpwards.tsx
@@ -2,40 +2,33 @@ import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
 export default function ArriveUpwards({
-  key,
-  disableAnimation = false,
+  keyBy,
   distance = 30,
   delay = 1,
   duration = 1,
   children,
-  disable = false,
   once = true,
   className = "",
   exit = false,
 }: {
-  key: string;
-  disableAnimation?: boolean;
+  keyBy: string;
   distance?: number;
   delay?: number;
   duration?: number;
   children: ReactNode;
-  disable?: boolean;
   once?: boolean;
   className?: string;
   exit?: boolean;
 }) {
-  const durationCalc = disable ? 0 : duration;
-  const delayCalc = disable ? 0 : delay;
-
   return (
     <motion.div
-      key={key}
+      key={keyBy}
       className={className}
       initial={{ opacity: 0, y: distance }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{
-        duration: disableAnimation ? 0 : durationCalc,
-        delay: disableAnimation ? 0 : delayCalc,
+        duration,
+        delay,
       }}
       exit={exit ? { opacity: 0, y: distance } : {}}
       viewport={{ once }}
