4c9526a7fbd0c9eff3e04f3eac682edc53bda4b0
CodeBRAVO <johnnypovolny@gmail.com>
2025-01-19 00:35:52 -0800

    Sick looking version of the card zoom! Going to tinker a bit with getting it to animate back out smoothly

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,73 +1,75 @@
 import cx from "classnames";
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useRef } from "react";
 
+import { useSystemStore } from "../state/system";
 import { ProjectObject } from "../types/components";
 
 export default function ProjectCard({ project }: { project: ProjectObject }) {
-  // const isClicked = useRef(false);
-  const [isOpen, setIsOpen] = useState(false);
-
+  const { openProjectId, setOpenProjectId, setClickedCardBoundingBox } =
+    useSystemStore();
+  const cardRef = useRef<HTMLDivElement>(null);
   const {
     _id,
     thumbnailAsset: { url = "" },
     thumbnailAlt = "",
     thumbnail_overlay_color = "",
   } = project;
+  const isOpen = openProjectId === _id;
 
-  const roundingClass = "rounded-2xl";
-
+  // We use state and then an effect to turn off the hover so that the bounding box will be the right size and not
+  // stretch the ProjectModal that will render and grows based on the "openProjectId"
   const onClick = async () => {
-    setIsOpen(true);
+    if (cardRef.current) {
+      const boundingBox = cardRef.current.getBoundingClientRect();
+      setClickedCardBoundingBox(boundingBox);
+      setOpenProjectId(_id);
+    }
   };
 
   const imageVariants = {
-    hover: { scale: 1.05, transition: { duration: 0.7 } },
+    hover: {
+      scale: 1.05,
+      transition: { duration: 0.7 },
+    },
   };
+  const roundingClass = "rounded-2xl";
+
   return (
     <motion.div
-      key={_id}
+      ref={cardRef}
       whileHover="hover"
       className={cx(
-        "flex grow-0 basis-[47%] cursor-pointer flex-col items-center justify-center overflow-hidden bg-transparent",
-        !isOpen && roundingClass,
-        isOpen && "z-50",
+        "relative grow-0 basis-[47%] cursor-pointer overflow-hidden bg-transparent",
+        roundingClass,
+        isOpen && "invisible",
       )}
       onClick={onClick}
       onTap={onClick}
-      layout
-      style={
-        isOpen
-          ? {
-              position: "fixed",
-              top: 0,
-              left: 0,
-              width: "100vw",
-              height: "100vh",
-              overscrollBehavior: "",
-            }
-          : {
-              position: "relative",
-            }
-      }
     >
+      {/* Image */}
       <motion.img
         src={url}
         alt={thumbnailAlt}
-        className="h-full w-full"
+        className="h-full w-full object-contain"
         variants={imageVariants}
-        style={isOpen ? { objectFit: "fill" } : { objectFit: "contain" }}
       />
+
+      {/* Overlay */}
       <motion.div
         className={cx(
-          `absolute inset-0 h-full w-full hover:!bg-transparent`,
-          !isOpen && roundingClass,
+          `absolute inset-0 flex h-full w-full flex-col items-center justify-center hover:!bg-transparent`,
+          roundingClass,
         )}
         style={{
           backgroundColor: thumbnail_overlay_color,
           mixBlendMode: "multiply",
         }}
-      />
+      ></motion.div>
+      {/* Content */}
+      <div className="absolute inset-0 flex h-full w-full flex-col items-center justify-center text-2xl text-stars-100">
+        I AM THE CARD
+      </div>
     </motion.div>
   );
 }
---
+++ src/components/ProjectModal.tsx
@@ -0,0 +1,151 @@
+import cx from "classnames";
+import {
+  AnimatePresence,
+  easeIn,
+  easeInOut,
+  easeOut,
+  motion,
+} from "framer-motion";
+import { pick } from "lodash";
+import { useEffect, useState } from "react";
+
+import { useSystemStore } from "../state/system";
+import { ProjectObject } from "../types/components";
+
+export default function ProjectModal({ project }: { project: ProjectObject }) {
+  const { setOpenProjectId, clickedCardBoundingBox } = useSystemStore();
+  const [didRenderModalCard, setDidRenderModalCard] = useState(false);
+  const [didAnimateOpen, setDidAnimateOpen] = useState(false);
+  const transitionDuration = 0.5;
+  const transitionDurationMs = transitionDuration * 1000;
+  const contentsDuration = 0.5;
+  const contentsDurationMs = contentsDuration * 1000;
+
+  const {
+    _id,
+    thumbnailAsset: { url = "" },
+    thumbnailAlt = "",
+  } = project;
+
+  // Animate in
+  useEffect(() => {
+    let animationTimeout = null;
+    setDidRenderModalCard(true);
+
+    if (clickedCardBoundingBox) {
+      animationTimeout = setTimeout(
+        () => setDidAnimateOpen(true),
+        transitionDuration,
+      );
+    }
+
+    return () => {
+      if (animationTimeout) {
+        clearTimeout(animationTimeout);
+      }
+    };
+  }, []);
+
+  // Animate out
+  const handleClose = async () => {
+    setDidAnimateOpen(false);
+
+    setTimeout(() => {
+      setDidRenderModalCard(false);
+      setTimeout(() => {
+        setOpenProjectId(null);
+      }, transitionDurationMs);
+    }, contentsDurationMs);
+  };
+
+  if (!clickedCardBoundingBox) {
+    return null;
+  }
+
+  const roundingClass = "rounded-2xl";
+
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={`project_${_id}_modal`}
+        className={cx(
+          "fixed z-10 cursor-pointer bg-stars-100",
+          !didAnimateOpen && roundingClass,
+        )}
+        onClick={handleClose}
+        onTap={handleClose}
+        layout
+        style={
+          !didRenderModalCard
+            ? pick(clickedCardBoundingBox, [
+                "top",
+                "left",
+                "right",
+                "bottom",
+                "width",
+                "height",
+              ])
+            : {
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                width: "100%",
+                height: "100%",
+              }
+        }
+        transition={{
+          layout: {
+            duration: transitionDuration,
+          },
+        }}
+      >
+        {/* Image */}
+        <AnimatePresence mode="wait">
+          {!didAnimateOpen ? (
+            <motion.img
+              key={`project_${_id}_modal_img`}
+              src={url}
+              alt={thumbnailAlt}
+              className={cx("h-full w-full", !didAnimateOpen && roundingClass)}
+              // initial={{ opacity: 0 }}
+              // animate={
+              //   didRenderModalCard
+              //     ? {
+              //         opacity: 1,
+              //         transition: { duration: transitionDuration },
+              //       }
+              //     : {}
+              // }
+              exit={{
+                opacity: 0,
+                transition: {
+                  delay: transitionDuration,
+                  duration: contentsDuration,
+                },
+              }}
+            />
+          ) : (
+            <motion.div
+              key={`project_${_id}_contents`}
+              className="flex h-full w-full flex-row items-center justify-center bg-dune-100"
+              initial={{ opacity: 0, y: -100 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: contentsDuration },
+              }}
+              exit={{
+                opacity: 0,
+                y: -100,
+                transition: { duration: contentsDuration },
+              }}
+            >
+              HELLO WORLD
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
