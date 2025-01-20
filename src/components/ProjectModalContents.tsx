ddc67484f74eab48039218b810245d1498a9643c
CodeBRAVO <johnnypovolny@gmail.com>
2025-01-19 23:03:38 -0800

    Fairly smooth transition to video now (shrinking but not with animation yet)

--- src/components/ProjectModalContents.tsx
+++ src/components/ProjectModalContents.tsx
@@ -1,5 +1,5 @@
 import cx from "classnames";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 import { useDisplay } from "../hooks/display";
@@ -8,6 +8,7 @@ import {
   ResolvedImageRef,
   ResolvedVideoRef,
 } from "../types/components";
+import Header from "./Header";
 import VideoBlockFullscreen from "./VideoBlockFullscreen";
 
 export default function ProjectModalContents({
@@ -40,30 +41,40 @@ export default function ProjectModalContents({
   const alt = thumbnail?.alt;
 
   return (
-    <motion.div className="absolute bottom-0 left-0 right-0 top-0 overflow-y-scroll overscroll-none text-night-400 scrollbar-hide">
-      {thumbnailUrl && (
-        <motion.img
-          key={`project_${_id}_modal_img`}
-          src={`${thumbnailUrl}?w=${innerWidth}&fit=clip&auto=format`}
-          className={cx(
-            "absolute bottom-0 left-0 right-0 top-0 h-full w-full",
-            !didAnimateOpen && roundingClass,
-            !didAnimateOpen || !video || !videoLoaded ? "visible" : "invisible",
+    <motion.div className="bg-night-gradient absolute bottom-0 left-0 right-0 top-0 overflow-y-scroll overscroll-none text-night-400 scrollbar-hide">
+      <Header isHomePage={false} isPortfolio={true} />
+      <AnimatePresence>
+        {thumbnailUrl && (
+          <motion.img
+            key={`project_${_id}_modal_img`}
+            src={`${thumbnailUrl}?w=${innerWidth}&fit=clip&auto=format`}
+            className={cx(
+              "absolute bottom-0 left-0 right-0 top-0 h-full w-full",
+              !didAnimateOpen && roundingClass,
+              !didAnimateOpen || !video || !videoLoaded
+                ? "visible"
+                : "invisible",
+            )}
+            alt={alt}
+          />
+        )}
+        <motion.div
+          key={`project_${_id}_modal_video`}
+          className="flex h-screen w-screen items-center justify-center"
+        >
+          {video && (
+            <VideoBlockFullscreen
+              video={video}
+              thumbnail={thumbnail}
+              setVideoLoaded={setVideoLoaded}
+              className={cx(
+                didAnimateOpen && videoLoaded ? "visible" : "invisible",
+                "w-11/12",
+              )}
+            />
           )}
-          alt={alt}
-        />
-      )}
-      {/* {video && (
-        <VideoBlockFullscreen
-          video={video}
-          thumbnail={thumbnail}
-          videoLoaded={videoLoaded}
-          setVideoLoaded={setVideoLoaded}
-          className={cx(
-            didAnimateOpen && videoLoaded ? "visible" : "invisible",
-          )}
-        />
-      )} */}
+        </motion.div>
+      </AnimatePresence>
     </motion.div>
   );
 }
