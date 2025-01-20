45173bf46a9736890ac37ddef4b8c4cad4c24d20
CodeBRAVO <johnnypovolny@gmail.com>
2025-01-19 23:03:38 -0800

    Fairly smooth transition to video now (shrinking but not with animation yet)

--- src/components/VideoBlockFullscreen.tsx
+++ src/components/VideoBlockFullscreen.tsx
@@ -1,43 +1,35 @@
 import cx from "classnames";
-import { useState } from "react";
+import { motion } from "framer-motion";
 
 import { ResolvedImageRef, ResolvedVideoRef } from "../types/components";
 
 export default function VideoBlockFullscreen({
   video,
-  thumbnail,
-  videoLoaded,
   setVideoLoaded,
   className = "",
 }: {
   video: ResolvedVideoRef;
   thumbnail: ResolvedImageRef | undefined;
-  videoLoaded: boolean;
   setVideoLoaded: (videoLoaded: boolean) => void;
   className?: string;
 }) {
   const {
     asset: { url },
   } = video;
-  // const [imageAnimationComplete, setImageAnimationComplete] = useState(false);
 
   return (
-    <div className={cx("relative w-screen overflow-hidden")}>
-      <div className={cx(className, "z-20")}>
-        <video
-          className="object-scale-down"
-          controls={false}
-          autoPlay
-          muted
-          loop
-          onLoadedData={() => {
-            setVideoLoaded(true);
-          }}
-        >
-          <source src={url} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    </div>
+    <motion.video
+      className={cx("relative object-scale-down", className)}
+      controls={false}
+      autoPlay
+      muted
+      loop
+      onLoadedData={() => {
+        setVideoLoaded(true);
+      }}
+    >
+      <source src={url} type="video/mp4" />
+      Your browser does not support the video tag.
+    </motion.video>
   );
 }
