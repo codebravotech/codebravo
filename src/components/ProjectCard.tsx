82a6b714770873341f4b654e72a36ba6b9210c6c
CodeBRAVO <johnnypovolny@gmail.com>
2025-02-07 14:38:34 -0800

    Oops commenting error

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -52,7 +52,7 @@ export default function ProjectCard({
       transition={
         animationPhase === "MODAL_CLOSED"
           ? { layout: { duration: 0 } }
-            { layout: { duration: 0, ease: "easeOut", delay: 0.3 } }
+          : { layout: { duration: 0, ease: "easeOut", delay: 0.3 } }
       }
       onLayoutAnimationComplete={() => {
         const latestAnimationPhase = useSystemStore.getState().animationPhase;
